8074f7c4f03b973420f65ca4d94d22358d2d39f4
Garsell <71719630+Edi-ID@users.noreply.github.com>
2026-03-19 07:28:10 +0700

    Add files via upload

---
+++ set-dns.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/bash
+
+echo "🚀 OPTIMASI VPS MAX PERFORMANCE"
+
+# Unlock dulu (silent)
+chattr -i /etc/resolv.conf 2>/dev/null
+chattr -i /etc/sysctl.conf 2>/dev/null
+
+# DNS (silent)
+cat <<EOF > /etc/resolv.conf
+nameserver 1.1.1.1
+nameserver 8.8.8.8
+EOF
+
+echo "✅ DNS OK"
+
+# SYSCTL (tanpa output ke layar)
+cat <<EOF >> /etc/sysctl.conf
+
+# BBR
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+
+# TCP OPTIMIZATION
+net.core.rmem_max=67108864
+net.core.wmem_max=67108864
+net.ipv4.tcp_rmem=4096 87380 67108864
+net.ipv4.tcp_wmem=4096 65536 67108864
+
+net.ipv4.tcp_fastopen=3
+net.ipv4.tcp_mtu_probing=1
+net.ipv4.tcp_window_scaling=1
+net.ipv4.tcp_sack=1
+net.ipv4.tcp_timestamps=1
+
+# CONNECTION HANDLING
+net.core.netdev_max_backlog=250000
+net.core.somaxconn=4096
+net.ipv4.tcp_max_syn_backlog=8192
+net.ipv4.tcp_syncookies=1
+
+# PORT & TIME_WAIT
+net.ipv4.ip_local_port_range=1024 65535
+net.ipv4.tcp_fin_timeout=15
+net.ipv4.tcp_tw_reuse=1
+
+EOF
+
+# Apply tanpa output
+sysctl -p > /dev/null 2>&1
+
+echo "✅ SYSCTL OPTIMIZED (silent)"
+
+# Lock
+chattr +i /etc/resolv.conf 2>/dev/null || true
+chattr +i /etc/sysctl.conf 2>/dev/null || true
+
+echo "🔥 VPS SIAP TEMPUR!"
